038eec3215ce6c784a0c35083f040c67f87a5379
Patryk Zawadzki <patrys@room-303.com>
2007-08-04 19:02:45 +0000

    - launch seahorse if specified
    Changed files:
        gnupg-agent.sh -> 1.8

--- gnupg-agent.sh
+++ gnupg-agent.sh
@@ -7,9 +7,10 @@ else
 fi
 
 X11=no
+seahorse=no
 if [ -r "${HOME}/.gnupg/gpg-agent.conf" ]; then
     grep -qE "^[[:blank:]]*pinentry-program[[:blank:]]*.*pinentry-(qt|gtk)" "${HOME}/.gnupg/gpg-agent.conf" && X11=yes
-    grep -qE "^[[:blank:]]*pinentry-program[[:blank:]]*.*seahorse-agent" "${HOME}/.gnupg/gpg-agent.conf" && X11=yes
+    grep -qE "^[[:blank:]]*pinentry-program[[:blank:]]*.*seahorse-agent" "${HOME}/.gnupg/gpg-agent.conf" && X11=yes && seahorse=yes
 fi
 
 if grep -q "^[[:blank:]]*use-agent" ${CFG} 2>/dev/null; then
@@ -17,7 +18,11 @@ if grep -q "^[[:blank:]]*use-agent" ${CFG} 2>/dev/null; then
 	export GPG_AGENT_INFO="$(cat ${HOME}/.gnupg/GPG_AGENT_INFO)"
     else
 	if [ -n "$DISPLAY" -a "$X11" = "yes" ] || [ -z "$DISPLAY" -a "$X11" = "no" ]; then
-	    eval "$(gpg-agent --daemon)"
+            if [ "$seahorse" = "no" ]; then
+                eval "$(gpg-agent --daemon)"
+            else
+                eval "$(seahorse-agent --variables)"
+            fi
 	    echo $GPG_AGENT_INFO > ~/.gnupg/GPG_AGENT_INFO
 	    export GPG_AGENT_INFO
 	fi
